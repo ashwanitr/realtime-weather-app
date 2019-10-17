cfbd7c01e834e6b0658cc8efd58b0c6628c14668
Ashwani <ashwanitr001@gmail.com>
2019-10-17 16:56:15 +0530

    setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -15,7 +15,7 @@ const location =searchElement.value
 
 messageOne.textConten="Loading......"
 
-fetch(`http://localhost:3000/weather?address=${location}`).then((response) =>{
+fetch(`/weather?address=${location}`).then((response) =>{
     response.json().then((data)=>{
        if (data.error){
            messageOne.textContent=data.error
